278b5e1d5737b5c202ae5ca538d7948c828f744f
Your Name <you@example.com>
2024-12-28 01:18:16 -0300

    Update Next.js configuration to conditionally set basePath and assetPrefix for GitHub Actions deployment
    - Added logic to configure basePath and assetPrefix based on the GITHUB_ACTIONS environment variable.
    - Removed previous hardcoded values for production, allowing for dynamic asset management in GitHub Actions.
    - Maintained existing configuration for local and production environments.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  ...(process.env.NODE_ENV === 'production' ? {
-    basePath: '/__',
-    assetPrefix: '/__/',
-  } : {}),
-  trailingSlash: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     remotePatterns: [
       {
