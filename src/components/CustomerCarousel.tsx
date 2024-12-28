eedb236bec5e774556df1a0fa6fdea4aff33d1c8
Your Name <you@example.com>
2024-12-28 00:50:40 -0300

    Refactor Next.js configuration and update image paths in CustomerCarousel component
    - Removed basePath and assetPrefix from next.config.js to simplify configuration.
    - Updated image paths in CustomerCarousel component to use relative paths for consistency.
    - Maintained existing client data structure and YouTube links.

--- src/components/CustomerCarousel.tsx
+++ src/components/CustomerCarousel.tsx
@@ -6,27 +6,27 @@ const CustomerCarousel: React.FC = () => {
   const clients = [
     { 
       name: 'Otsuka', 
-      image: '/_/clients/otsuka.jpg',
+      image: './clients/otsuka.jpg',
       youtube: 'https://www.youtube.com/@OtsukaXD'
     },
     { 
       name: 'F0rsaken', 
-      image: '/_/clients/f0rsaken.jpg',
+      image: './clients/f0rsaken.jpg',
       youtube: 'https://www.youtube.com/@f0rsaken'
     },
     { 
       name: 'Nuuhfps', 
-      image: '/_/clients/nuuhfps.jpg',
+      image: './clients/nuuhfps.jpg',
       youtube: 'https://www.youtube.com/@Nuuhfps'
     },
     { 
       name: 'AAABW', 
-      image: '/_/clients/aaabw.jpg',
+      image: './clients/aaabw.jpg',
       youtube: 'https://www.youtube.com/@aaabw_'
     },
     { 
       name: 'Brunowski', 
-      image: '/_/clients/iBrunowski.jpg',
+      image: './clients/iBrunowski.jpg',
       youtube: 'https://www.youtube.com/@ibrunowskivalorant'
     }
   ];
